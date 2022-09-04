96fbbb19babd0987a85b6d1ecb01024b7a7906af
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-09-04 10:31:12 +0200

    added at home page h1 tag, and made some responsive page changes.

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,16 +3,38 @@ import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div className=' '>
+    <div className='relative'>
       <Navbar />
-      <div className=''>
+      <div className='relative'>
         <div className='absolute t-0 l-0 r-0 bg-black/80 w-screen h-[700px]'></div>
         <img
           className='w-screen  h-[700px]'
           src='computer-g971e436ec_1920.jpg'
           alt='Officebook'
         />
+        <div className='absolute top-48 sm:top-52 w-full text-white  text-center'>
+          <h1 className='text-5xl mb-6 font-bold tracking-wide'>
+            Create a Free Website
+          </h1>
+          <h3 className='font-semibold text-2xl mb-4'>
+            Everything you need right in the browser.
+          </h3>
+          <p className='tracking-wide text-lg'>
+            Build your own website with{' '}
+            <span className='text-red-300 font-semibold cursor-pointer hover:opacity-90'>
+              W3Schools Spaces.
+            </span>
+            <span className=''>It is easy to use -try it!</span>
+          </p>
+          <button
+            submit
+            className='text-white bottom-0 right-0 px-14 py-3 hover:bg-[#059862] bg-[#04aa6d] rounded-full mt-8 text-lg font-semibold tracking-wide mb-8'
+          >
+            Get started for free
+          </button>
+        </div>
       </div>
+
       <div>
         <h1>Get Started here</h1>
       </div>
--- src/pages/login.js
+++ src/pages/login.js
@@ -3,33 +3,37 @@ import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative w-[90%] m-auto'>
       <Link to='/' className=''>
-        <img src='w3chool.png' alt='logo' className=' m-4  w-16 h-14' />
+        <img
+          src='w3chool.png'
+          alt='logo'
+          className=' my-4  w-16 h-14 hover:opacity-90'
+        />
       </Link>
       <div className='flex justify-center items-center mt-32'>
-        <form className='flex flex-col p-8 center border-2  border-[#e3e3e3] shadow-md w-[350px] h-auto justify-center  '>
+        <form className='flex flex-col md:w-[450px] p-8 center border-2  border-[#e3e3e3] shadow-md w-[350px] h-auto justify-center  '>
           <h1 className='mb-8 text-2xl font-bold'>Log in</h1>
           <label className='fontsemibold text-lg'>email</label>
           <input
             type='email'
             placeholder='email'
-            className='p-2 shadow-md border-2 border-[#ced4da] mb-4'
+            className='p-2 focus:shadow-sm border-2 border-[#ced4da] mb-4 outline-none'
           />
           <label>email</label>
           <input
             type='password'
             placeholder='password'
-            className='border-2 shadow-md border-[#ced4da] p-2'
+            className='border-2 focus:shadow-sm border-[#ced4da] p-2 outline-none'
           />
           <button
             submit
-            className='mt-12 bg-[#04aa6d] text-white p-2 rounded-full font-semibold tracking-wide text-lg'
+            className='mt-12 bg-[#04aa6d] hover:bg-[#059862]  text-white p-2 rounded-full font-semibold tracking-wide text-lg'
           >
             <Link to='/login'>Log in</Link>
           </button>
         </form>{' '}
-        <button className='text-white absolute bottom-0 right-0 px-8 py-2 border-2 bg-[#04aa6d] rounded-full m-4 text-lg tracking-wide mb-8'>
+        <button className='text-white absolute bottom-0 right-0 px-8 py-2 border-2 hover:bg-[#059862] bg-[#04aa6d] rounded-full m-4 text-lg tracking-wide mb-8'>
           Help
         </button>
       </div>
