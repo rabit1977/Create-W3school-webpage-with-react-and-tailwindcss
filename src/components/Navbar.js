8f12d0608d1eeb5e86807180e39b7859a3560c0b
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-09-17 15:50:10 +0200

    added some more tailwind stuff to this project.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div className='flex w-full mx-auto absolute justify-between items-center z-50'>
+    <div className='flex w-full p-4 md:p-6 absolute justify-between items-center z-50'>
       <Link to='/' className=''>
-        <img src='w3chool.png' alt='logo' className=' m-4  w-16 h-14' />
+        <img src='w3chool.png' alt='logo' className='w-16 h-14' />
       </Link>
       <Link to='/login'>
-        <button className='text-white px-12 py-2 border-2 border-[#04aa6d] rounded-full hover:bg-black/60 transition duration-200  m-4 text-lg'>
+        <button className='text-white px-8 sm:px-10 md:px-12 py-1.5 sm:py-2 border-2 border-[#04aa6d] rounded-full hover:bg-black/20 transition duration-200 text-md sm:text-lg'>
           Log in
         </button>
       </Link>
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,21 +5,21 @@ export default function Home() {
   return (
     <div className='relative'>
       <Navbar />
-      <div className='relative'>
-        <div className='absolute t-0 l-0 r-0 bg-black/80 w-screen h-[700px]'></div>
+      <div className='relative overflow-hidden'>
+        <div className='absolute bg-[#282a35e6] w-screen h-[calc(100vh-6rem)]'></div>
         <img
-          className='w-screen  h-[700px]'
+          className=' h-[calc(100vh-6rem)] w-screen'
           src='computer-g971e436ec_1920.jpg'
           alt='Officebook'
         />
-        <div className='absolute top-48 sm:top-52 w-full text-white  text-center'>
-          <h1 className='text-5xl mb-6 font-bold tracking-wide'>
+        <div className='absolute top-56 sm:top-52 w-full text-white  text-center'>
+          <h1 className='text-3xl sm:text-4xl md:text-5xl mb-6 font-bold tracking-wide'>
             Create a Free Website
           </h1>
-          <h3 className='font-semibold text-2xl mb-4'>
+          <h3 className='font-semibold md:text-2xl sm:text-xl text-lg mb-4'>
             Everything you need right in the browser.
           </h3>
-          <p className='tracking-wide text-lg'>
+          <p className='tracking-wide text-sm sm:text-md md-text-lg'>
             Build your own website with{' '}
             <span className='text-red-300 font-semibold cursor-pointer hover:opacity-90'>
               W3Schools Spaces.
@@ -28,15 +28,61 @@ export default function Home() {
           </p>
           <button
             submit
-            className='text-white bottom-0 right-0 px-14 py-3 hover:bg-[#059862] bg-[#04aa6d] rounded-full mt-8 text-lg font-semibold tracking-wide mb-8'
+            className='text-white bottom-0 sm:px-12 md:px-14 px-10 sm:py-2.5 md:py-3 py-2 hover:bg-[#059862] bg-[#04aa6d] rounded-full mt-8 sm:text-lg text-md font-normal md:font-medium tracking-wide mb-8'
           >
             Get started for free
           </button>
         </div>
       </div>
-
-      <div>
-        <h1>Get Started here</h1>
+      <div className='grid grid-cols-1 gap-8 w-5/6 m-auto text-center my-12 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='p-4'>
+          <h1 className='text-[#04aa6d] font-semibold text-lg md:text-xl mb-1 tracking-wide'>
+            Website
+          </h1>
+          <p className='text-xs md:text-sm text-slate-800'>
+            Create your own website and share it with others
+          </p>
+        </div>
+        <div className='p-4'>
+          <h1 className='text-[#04aa6d] font-semibold text-lg md:text-xl mb-1 tracking-wide'>
+            Code editor
+          </h1>
+          <p className='text-xs md:text-sm text-slate-800'>
+            Code directly in your browser
+          </p>
+        </div>
+        <div className='p-4'>
+          <h1 className='text-[#04aa6d] font-semibold text-lg md:text-xl mb-1 tracking-wide'>
+            Templates
+          </h1>
+          <p className='text-xs md:text-sm text-slate-800'>
+            Start with ready-to-use designs and structures
+          </p>
+        </div>
+        <div className='p-4'>
+          <h1 className='text-[#04aa6d] font-semibold text-lg md:text-xl mb-1 tracking-wide'>
+            File manager
+          </h1>
+          <p className='text-xs md:text-sm text-slate-800'>
+            Upload and handle files in your browser
+          </p>
+        </div>
+        <div className='p-4'>
+          <h1 className='text-[#04aa6d] font-semibold text-lg md:text-xl mb-1 tracking-wide'>
+            Experiment
+          </h1>
+          <p className='text-xs md:text-sm text-slate-800'>
+            Learn, test, and go live with your space
+          </p>
+        </div>
+        <div className='p-4'>
+          <h1 className='text-[#04aa6d] font-semibold text-lg md:text-xl mb-1 tracking-wide'>
+            Playground
+          </h1>
+          <p className='text-xs md:text-sm text-slate-800'>
+            Playground for HTML, CSS and JavaScript
+          </p>
+        </div>
       </div>
     </div>
   );
